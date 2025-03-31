e94d878ca5344df9eb3bcd521e441d44947d59d6
Philip Tofte Nielsen <philiptofte@hotmail.com>
2025-03-31 23:37:19 +0200

    which-key.lua

--- config/nvim/lua/plugins/plugins.lua
+++ config/nvim/lua/plugins/plugins.lua
@@ -1,8 +1,25 @@
 return {
-{
-  "ibhagwan/fzf-lua",
-  dependencies = { "echasnovski/mini.icons" },
-  opts = {}
-}
-
+    {
+      "ibhagwan/fzf-lua",
+      dependencies = { "echasnovski/mini.icons" },
+      opts = {}
+    },
+    {
+     "folke/which-key.nvim",
+     event = "VeryLazy",
+     opts = {
+       -- your configuration comes here
+       -- or leave it empty to use the default settings
+       -- refer to the configuration section below
+     },
+     keys = {
+       {
+         "<leader>?",
+         function()
+           require("which-key").show({ global = false })
+         end,
+         desc = "Buffer Local Keymaps (which-key)",
+       },
+     },
+    }
 }

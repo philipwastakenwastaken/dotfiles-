2864adef30c37524ba8ad27f59abf6f3470331dc
Philip Tofte Nielsen <philiptofte@hotmail.com>
2025-04-02 20:55:36 +0200

    Fix replace_config for nested directories

--- replace_config.sh
+++ replace_config.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-fd -t d "" config -x rm -rf ~/.{}
-fd -t d "" config -d 1 -x ln -s $HOME/dotfiles/{} $HOME/.{}
+# Remove only top-level directories (e.g., nvim) from home
+fd -t d "" config -d 1 -x rm -rf ~/."{}"
+
+# Create symlinks for these top-level directories
+fd -t d "" config -d 1 -x ln -s $HOME/dotfiles/"{}" $HOME/."{}"
 
 # cp ~/dotfiles/config/starship.toml ~/.config/

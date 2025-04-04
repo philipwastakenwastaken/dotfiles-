07c3888c9ce97a077ee71be8121f625dae3f942c
Philip Tofte Nielsen <philiptofte@hotmail.com>
2025-04-04 22:09:04 +0200

    diagnostics

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,5 +1,20 @@
-local map = vim.keymap.set
-
--- Remap space as leader key
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+vim.keymap.set("n", "gl", vim.diagnostic.open_float, { noremap = true, silent = true, desc = "Show floating diagnostics window" })
+
+vim.keymap.set("n", "]d", function()
+  vim.diagnostic.jump({
+    count = 1,         -- move forward by one diagnostic
+    wrap = true,       -- wrap around if at the end of the file
+    float = true,      -- open a floating window after jumping
+  })
+end, { noremap = true, silent = true, desc = "Jump to next diagnostic" })
+
+vim.keymap.set("n", "[d", function()
+  vim.diagnostic.jump({
+    count = -1,         -- move forward by one diagnostic
+    wrap = true,       -- wrap around if at the end of the file
+    float = true,      -- open a floating window after jumping
+  })
+end, { noremap = true, silent = true, desc = "Jump to previous diagnostic" })

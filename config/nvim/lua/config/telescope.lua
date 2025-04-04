43ee68003efb059e77095f51132c18573e3eeb8e
Philip Tofte Nielsen <philiptofte@hotmail.com>
2025-04-05 00:28:35 +0200

    more advanced buffer find

--- config/nvim/lua/config/telescope.lua
+++ config/nvim/lua/config/telescope.lua
@@ -1,4 +1,5 @@
 local telescope = require("telescope")
+local actions = require("telescope.actions")
 
 telescope.setup({
 	extensions = {
@@ -18,5 +19,20 @@ end)
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
-vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+
+vim.keymap.set("n", "<leader>fb", function()
+	builtin.buffers({
+		initial_mode = "normal",
+		sort_mru = true,
+		sort_lastused = true,
+		attach_mappings = function(_, map)
+			-- In insert mode, press Ctrl-d to delete the selected buffer
+			map("i", "<C-d>", actions.delete_buffer)
+			-- In normal mode, press 'd' to delete the selected buffer
+			map("n", "d", actions.delete_buffer)
+			return true
+		end,
+	})
+end, { desc = "Telescope buffers" })
+
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })

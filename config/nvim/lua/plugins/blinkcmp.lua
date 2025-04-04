b1d464802283899080a469273eab3b6f82c4413b
Philip Tofte Nielsen <philiptofte@hotmail.com>
2025-04-04 21:23:28 +0200

    ghosttext + enter keymap

--- config/nvim/lua/plugins/blinkcmp.lua
+++ config/nvim/lua/plugins/blinkcmp.lua
@@ -8,15 +8,24 @@ return {
     build = 'cargo build --release',
     opts = {
       -- Choose a preset for key mappings: 'default', 'super-tab', 'enter', or 'none'
-      keymap = { preset = 'default' },
+      keymap = { preset = 'enter' },
       appearance = {
         nerd_font_variant = 'mono', -- Adjust based on your preferred Nerd Font variant
       },
-      completion = { documentation = { auto_show = true } },
+      completion = { documentation = { auto_show = true },
+        ghost_text = {
+            enabled = true,
+            show_with_selection = true,
+            show_without_selection = false,
+            show_with_menu = true,
+            show_without_menu = true,
+      },
+            },
       sources = {
         default = { 'lsp', 'path', 'snippets', 'buffer' },
       },
-      fuzzy = { implementation = "rust" }
+      fuzzy = { implementation = "rust" },
+      signature = { enabled = true }
     },
     opts_extend = { "sources.default" }
   }

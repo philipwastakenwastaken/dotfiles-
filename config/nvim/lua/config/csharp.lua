1861a2ddf5b401eb816f52227c0a1dce5ab5e31b
Philip Tofte Nielsen <pni@cvation.com>
2025-04-08 11:01:37 +0200

    Don't use .git for root candidate

--- config/nvim/lua/config/csharp.lua
+++ config/nvim/lua/config/csharp.lua
@@ -3,7 +3,7 @@ local capabilities = require("blink.cmp").get_lsp_capabilities()
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = "cs",
 	callback = function(args)
-		local root_candidates = vim.fs.find({ ".sln", ".csproj", ".git" }, { upward = true })
+		local root_candidates = vim.fs.find({ ".sln", ".csproj" }, { upward = true })
 		local root_dir = root_candidates[1] and vim.fs.dirname(root_candidates[1]) or vim.loop.cwd()
 
 		vim.lsp.start({

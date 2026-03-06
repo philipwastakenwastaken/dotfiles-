0a61af34839cb25fbb3735be8cf3281f542d9bfe
Philip Tofte Nielsen <pni@cvation.com>
2026-03-06 15:23:31 +0100

    updated nvim

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -21,19 +21,12 @@ return {
 			},
 		},
 		config = function()
-			local lspconfig = require("lspconfig")
-			local util = require("lspconfig.util")
-			-- Use blink.cmp to get the enhanced LSP capabilities
 			local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-			lspconfig.pyright.setup{}
+			-- Apply capabilities globally to all servers
+			vim.lsp.config("*", { capabilities = capabilities })
 
-			lspconfig.ruff.setup{}
-
-			lspconfig.rust_analyzer.setup{}
-
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
+			vim.lsp.config("lua_ls", {
 				on_init = function(client)
 					if client.workspace_folders then
 						local path = client.workspace_folders[1].name
@@ -46,22 +39,10 @@ return {
 					end
 
 					client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
-						runtime = {
-							-- Tell the language server which version of Lua you're using
-							-- (most likely LuaJIT in the case of Neovim)
-							version = "LuaJIT",
-						},
-						-- Make the server aware of Neovim runtime files
+						runtime = { version = "LuaJIT" },
 						workspace = {
 							checkThirdParty = false,
-							library = {
-								vim.env.VIMRUNTIME,
-								-- Depending on the usage, you might want to add additional paths here.
-								-- "${3rd}/luv/library"
-								-- "${3rd}/busted/library",
-							},
-							-- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
-							-- library = vim.api.nvim_get_runtime_file("", true)
+							library = { vim.env.VIMRUNTIME },
 						},
 					})
 				end,
@@ -70,59 +51,45 @@ return {
 				},
 			})
 
-			local root_candidates = vim.fs.find({ ".sln", ".csproj" }, { upward = true })
-			local root_dir = root_candidates[1] and vim.fs.dirname(root_candidates[1]) or vim.loop.cwd()
-			lspconfig.bicep.setup({
+			vim.lsp.config("bicep", {
 				cmd = { "bicep-langserver" },
-				capabilities = capabilities,
-				root_dir = root_dir,
+				root_dir = function(bufnr, cb)
+					local root = vim.fs.root(bufnr, { ".sln", ".csproj" })
+					cb(root or vim.uv.cwd())
+				end,
 			})
 
-			-- Explicit configuration for jsonls
-			lspconfig.jsonls.setup({
-				capabilities = capabilities,
+			vim.lsp.config("jsonls", {
 				filetypes = { "json", "jsonc" },
 				settings = {
 					json = {
-						-- Define custom schemas. For example, use the package.json schema for package.json files:
 						schemas = {
 							{
 								fileMatch = { "package.json" },
 								url = "https://json.schemastore.org/package.json",
 							},
-							-- Add additional schemas here if needed.
 						},
 						validate = { enable = true },
 					},
 				},
 			})
 
-			-- absolute path of the wrapper placed in the Nix store
-			local pes_exe = vim.fn.exepath("powershell-editor-services") -- e.g. /nix/store/…/bin/powershell-editor-services
+			local pes_exe = vim.fn.exepath("powershell-editor-services")
 			local pes_root = vim.fn.fnamemodify(pes_exe, ":h:h") .. "/lib/powershell-editor-services"
 
-			lspconfig.powershell_es.setup({
-				-- (optional) tell lspconfig where the bundle lives; not strictly required
+			vim.lsp.config("powershell_es", {
 				bundle_path = pes_root,
-
-				-- filetypes you want the server on
 				filetypes = { "ps1", "psm1", "psd1" },
-
-				-- start only inside a Git repo or when that settings file exists
-				root_dir = util.root_pattern("PSScriptAnalyzerSettings.psd1", ".git"),
+				root_dir = function(bufnr, cb)
+					cb(vim.fs.root(bufnr, { "PSScriptAnalyzerSettings.psd1", ".git" }))
+				end,
 			})
 
-			local function find_pipelines_root(fname)
-				local base = util.root_pattern("pipelines")(fname)
-				if base then
-					return util.path.join(base, "pipelines")
-				end
-				return nil
-			end
-
-			lspconfig.azure_pipelines_ls.setup({
-				root_dir = find_pipelines_root,
-				capabilities = capabilities,
+			vim.lsp.config("azure_pipelines_ls", {
+				root_dir = function(bufnr, cb)
+					local root = vim.fs.root(bufnr, { "pipelines" })
+					cb(root and vim.fs.joinpath(root, "pipelines") or nil)
+				end,
 				settings = {
 					yaml = {
 						schemas = {
@@ -137,6 +104,17 @@ return {
 						},
 					},
 				},
+			})
+
+			vim.lsp.enable({
+				"pyright",
+				"ruff",
+				"rust_analyzer",
+				"lua_ls",
+				"bicep",
+				"jsonls",
+				"powershell_es",
+				"azure_pipelines_ls",
 			})
 		end,
 	},
